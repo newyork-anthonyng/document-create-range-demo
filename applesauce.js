e829fef525941bc3b2a1651aa01bbfa610c728a2
Anthony Ng <ang@wsgc.com>
2019-06-25 14:16:22 -0400

    Refactor

--- applesauce.js
+++ applesauce.js
@@ -16,37 +16,46 @@ function applesauce() {
     if ($textNode === undefined) {
         return;
     }
-    // split text into an array
-    const textArray = $textNode.textContent.split(" ");
+
+    highlightTextForNode($textNode);
+}
+
+function highlightTextForNode($node) {
+    const textArray = $node.textContent.split(" ");
     let index = 0;
 
-    // iterate through array
     for (let i = 0; i < textArray.length; i++) {
         const currentWord = textArray[i];
 
-        // see if element matches a blacklist word
-
-        if (BLACKLIST_WORDS.includes(currentWord.toLowerCase())) {
-            // if element does match, create a range for it
-            let start = index;
-            let end = start + currentWord.length;
-
-            const range = document.createRange();
-            range.setStart($textNode, start);
-            range.setEnd($textNode, end);
-
-            // use the range's client rect and highlight it
-            const clientRects = range.getClientRects();
-            const $highlight = document.createElement("div");
-            $highlight.classList.add("highlight");
-            $highlight.style.position = "absolute";
-            $highlight.style.left = `${clientRects[0].left}px`;
-            $highlight.style.top = `${clientRects[0].top}px`;
-            document.body.appendChild($highlight);
+        if (isBlacklist(currentWord)) {
+            const start = index;
+            const end = start + currentWord.length;
+            const clientRects = getClientRectsForRange($node, start, end);
+            renderWordHighlight(clientRects);
         }
 
         index += currentWord.length + 1;
     }
+}
+
+function getClientRectsForRange($node, start, end) {
+    const range = document.createRange();
+    range.setStart($node, start);
+    range.setEnd($node, end);
+    return range.getClientRects();
+}
+
+function isBlacklist(word) {
+    return BLACKLIST_WORDS.includes(word.toLowerCase());
+}
+
+function renderWordHighlight(clientRects) {
+    const $highlight = document.createElement("div");
+    $highlight.classList.add("highlight");
+    $highlight.style.position = "absolute";
+    $highlight.style.left = `${clientRects[0].left}px`;
+    $highlight.style.top = `${clientRects[0].top}px`;
+    document.body.appendChild($highlight);
 }
 
 applesauce();
